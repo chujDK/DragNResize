b49cd8ed52a3a819ff6c60eac5e54b662e1daea5
chujDK <1768485949@qq.com>
2023-02-05 14:22:58 +0800

    feat: add resize
    现在存在的问题是无法 block 右键抬起。
    不使用 <WIN> 作为 mod 键而是使用 <CTRL> 也是出于这个问题。

--- src/DragNResize.cpp
+++ src/DragNResize.cpp
@@ -12,7 +12,20 @@ class MKHook
         return hook;
     }
 
-    int message()
+    int InstallHook()
+    {
+        if ((MKHook::get().hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHook, NULL, 0)) == NULL)
+        {
+            return 1;
+        }
+        if ((MKHook::get().hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHook, NULL, 0)) == NULL)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    int MessageLoop()
     {
         while (msg.message != WM_QUIT)
         {
@@ -42,32 +55,52 @@ class MKHook
     void ModDown(bool t)
     {
         modDown = t;
-    }
-
-    void LButtonDown(bool t)
-    {
-        if (modDown)
+        if (!t)
         {
-            lButtonDown = t;
+            active = t;
         }
     }
 
-    void RButtonDown(bool t)
+    void DragButtonDown(bool t)
     {
         if (modDown)
         {
-            rButtonDown = t;
+            dragButtonDown = t;
+            if (t)
+            {
+                active = t;
+            }
+            else
+            {
+                active = Resizing();
+            }
+        }
+    }
+
+    void ResizeButtonDown(bool t)
+    {
+        if (modDown)
+        {
+            resizeButtonDown = t;
+            if (t)
+            {
+                active = t;
+            }
+            else
+            {
+                active = Dragging();
+            }
         }
     }
 
     bool Dragging() const
     {
-        return modDown && lButtonDown;
+        return modDown && dragButtonDown;
     }
 
     bool Resizing() const
     {
-        return modDown && rButtonDown;
+        return modDown && resizeButtonDown;
     }
 
     void SetCursor(const POINT &p)
@@ -96,13 +129,30 @@ class MKHook
         return rightBottom;
     }
 
+    void SetResizeWindow(HWND h)
+    {
+        resizeWindow = h;
+    }
+    HWND ResizeWindow() const
+    {
+        return resizeWindow;
+    }
+
+    bool IsActive() const
+    {
+        return active;
+    }
+
   private:
     MSG msg;
     bool modDown = false;
-    bool lButtonDown = false;
-    bool rButtonDown = false;
+    bool dragButtonDown = false;
+    bool resizeButtonDown = false;
 
     POINT cursor{}, leftTop{}, rightBottom{};
+    HWND resizeWindow = NULL;
+
+    bool active = false; // are we dragging or resizing?
 };
 
 static bool IsFullscreen(HWND windowHandle)
@@ -118,32 +168,57 @@ static bool IsFullscreen(HWND windowHandle)
            windowRect.top == monitorInfo.rcMonitor.top && windowRect.bottom == monitorInfo.rcMonitor.bottom;
 }
 
+// return 0 on success
+static int RestoreAndForegroundWindow(HWND hWindow)
+{
+    // if window is maximized, restore it first
+    auto style = GetWindowLongPtr(hWindow, GWL_STYLE);
+    if (style != 0)
+    {
+        if (style & WS_MAXIMIZE)
+        {
+            ShowWindow(hWindow, SW_RESTORE);
+        }
+        SetForegroundWindow(hWindow);
+        return 0;
+    }
+    return 1;
+}
+
 LRESULT MouseHook(int code, WPARAM wParam, LPARAM lParam)
 {
     MKHook &mkHook = MKHook::get();
+    auto active = mkHook.IsActive();
     if (code == 0 && lParam != NULL) // FIXME: when will lParam be NULL?
     {
         MSLLHOOKSTRUCT *pMouseStruct = (MSLLHOOKSTRUCT *)lParam;
+        bool moveCursorToRightBottom = false;
         if (wParam == WM_LBUTTONDOWN)
         {
-            mkHook.LButtonDown(true);
-            // maybe we will occur a drag?
-            if (mkHook.Dragging())
+            mkHook.DragButtonDown(true);
+            // we will occur a drag
+            if (mkHook.Dragging() && !active)
             {
                 mkHook.SetCursor(pMouseStruct->pt);
             }
         }
         else if (wParam == WM_LBUTTONUP)
         {
-            mkHook.LButtonDown(false);
+            mkHook.DragButtonDown(false);
         }
         else if (wParam == WM_RBUTTONDOWN)
         {
-            mkHook.RButtonDown(true);
+            mkHook.ResizeButtonDown(true);
+            if (mkHook.Resizing() && !active)
+            {
+                // if we enter the resizing state, we need move cursor to the right bottom of the window later. also
+                // as the cursor will go outside of the window. we need to remember the window's handle
+                moveCursorToRightBottom = true;
+            }
         }
         else if (wParam == WM_RBUTTONUP)
         {
-            mkHook.RButtonDown(false);
+            mkHook.ResizeButtonDown(false);
         }
 
         if (lParam != NULL)
@@ -156,36 +231,47 @@ LRESULT MouseHook(int code, WPARAM wParam, LPARAM lParam)
                 // 2. fullscreen application
                 if (hWindow != NULL && hWindow != GetDesktopWindow() && !IsFullscreen(hWindow))
                 {
-                    // if window is maximized, restore it first
-                    auto style = GetWindowLongPtr(hWindow, GWL_STYLE);
-                    if (style != 0)
+                    if (mkHook.Dragging())
                     {
-                        if (style & WS_MAXIMIZE)
-                        {
-                            ShowWindow(hWindow, SW_RESTORE);
-                        }
-                        SetForegroundWindow(hWindow);
-
+                        RestoreAndForegroundWindow(hWindow);
                         RECT windowRect;
                         GetWindowRect(hWindow, &windowRect);
-                        if (mkHook.Dragging())
-                        {
 
-                            auto dMouseX = pMouseStruct->pt.x - mkHook.Cursor().x;
-                            auto dMouseY = pMouseStruct->pt.y - mkHook.Cursor().y;
-                            if (SetWindowPos(hWindow, HWND_TOP, windowRect.left + dMouseX, windowRect.top + dMouseY, 0,
-                                             0, SWP_NOSIZE | SWP_DRAWFRAME))
+                        auto dMouseX = pMouseStruct->pt.x - mkHook.Cursor().x;
+                        auto dMouseY = pMouseStruct->pt.y - mkHook.Cursor().y;
+                        if (SetWindowPos(hWindow, HWND_TOP, windowRect.left + dMouseX, windowRect.top + dMouseY, 0, 0,
+                                         SWP_NOSIZE))
+                        {
+                            // only move cursor when the window is moved
+                            if (SetCursorPos(pMouseStruct->pt.x, pMouseStruct->pt.y))
                             {
-                                // only move cursor when the window is moved
-                                if (SetCursorPos(pMouseStruct->pt.x, pMouseStruct->pt.y))
-                                {
-                                    mkHook.SetCursor(pMouseStruct->pt);
-                                }
+                                mkHook.SetCursor(pMouseStruct->pt);
                             }
+                        }
+                        return 1;
+                    }
+                    else if (mkHook.Resizing())
+                    {
+                        if (moveCursorToRightBottom)
+                        {
+                            RestoreAndForegroundWindow(hWindow);
+                            // ignore the movement in this case
+                            RECT windowRect;
+                            GetWindowRect(hWindow, &windowRect);
+                            SetCursorPos(windowRect.right, windowRect.bottom);
+                            // remember the window
+                            mkHook.SetResizeWindow(hWindow);
                             return 1;
                         }
-                        else if (mkHook.Resizing())
+                        RECT windowRect;
+                        GetWindowRect(mkHook.ResizeWindow(), &windowRect);
+                        auto width = pMouseStruct->pt.x - windowRect.left;
+                        auto height = pMouseStruct->pt.y - windowRect.top;
+                        if (SetWindowPos(mkHook.ResizeWindow(), HWND_TOP, windowRect.left, windowRect.top, width,
+                                         height, SWP_NOCOPYBITS))
                         {
+                            SetCursorPos(pMouseStruct->pt.x, pMouseStruct->pt.y);
+                            return 1;
                         }
                     }
                 }
@@ -221,13 +307,9 @@ LRESULT KeyboardHook(int code, WPARAM wParam, LPARAM lParam)
 
 int main()
 {
-    if ((MKHook::get().hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHook, NULL, 0)) == NULL)
+    if (MKHook::get().InstallHook() != 0)
     {
         return 1;
     }
-    if ((MKHook::get().hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHook, NULL, 0)) == NULL)
-    {
-        return 1;
-    }
-    return MKHook::get().message();
+    return MKHook::get().MessageLoop();
 }
