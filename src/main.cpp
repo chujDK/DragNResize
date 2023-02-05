4d0d08fd39b7a388cf0085590d8be23fd96391f4
chujDK <1768485949@qq.com>
2023-02-05 15:28:02 +0800

    misc: clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,6 @@
 
 #include <windows.h>
 
-/*
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
-{
-    if (MKHook::get().InstallHook() != 0)
-    {
-        return 1;
-    }
-    return MKHook::get().MessageLoop();
-}
-*/
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
 
@@ -27,27 +15,4 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     }
     return MKHook::get().MessageLoop();
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT: {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        // All painting occurs here, between BeginPaint and EndPaint.
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-        EndPaint(hwnd, &ps);
-    }
-        return 0;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
